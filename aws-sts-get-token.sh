e6e7e389d6783fdb0a6a8c6470381ba1eb896e43
Hleb Kanonik <esscyh@gmail.com>
2022-06-24 18:49:51 +0400

    fix typo

--- aws-sts-get-token.sh
+++ aws-sts-get-token.sh
@@ -20,7 +20,7 @@ then
 else
   `aws --profile $AWS_USER_PROFILE configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"`
   `aws --profile $AWS_USER_PROFILE configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"`
-  `aws --profile ec$AWS_USER_PROFILE configure set aws_session_token "$AWS_SESSION_TOKEN"`
+  `aws --profile $AWS_USER_PROFILE configure set aws_session_token "$AWS_SESSION_TOKEN"`
    
   echo "Temporary credentials configured!"
 fi
