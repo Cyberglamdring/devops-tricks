5d982ab75dd29396c921d67df349b2a43bc8f39a
cyberglamdring <esscyh@gmail.com>
2022-06-24 18:16:52 +0400

    AWS Login added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# devops-tricks
-DevOps Tips &amp; Tricks that make you life easier
+# DevOps Tips and Tricks
+
+## AWS Login
+
+1. Download script
+
+```bash
+curl -LO https://raw.githubusercontent.com/Cyberglamdring/devops-tricks/main/aws-sts-get-token.sh
+```
+
+2. Permisisons and Alias
+
+```bash
+chmod +x aws-sts-get-token.sh && alias awslogin="$HOME/aws-sts-get-token.sh"
+```
+
+3. Usage
+```bash
+awslogin
+```
---
+++ aws-sts-get-token.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+## HOW TO RUN & CREATE ALIAS
+# chmod +x aws-sts-get-token.sh
+# alias awslogin="$HOME/aws-sts-get-token.sh"
+
+read -p 'Profile (default): ' AWS_USER_PROFILE
+read -p 'MFA ARN (default arn:aws:iam::<aws_account_id>:mfa/<user>): ' ARN_OF_MFA
+read -p 'MFA Token (xxxxxx): ' MFA_TOKEN_CODE
+
+read AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< \
+$( aws --profile $AWS_USER_PROFILE sts get-session-token \
+  --serial-number $ARN_OF_MFA \
+  --token-code $MFA_TOKEN_CODE \
+  --output text  | awk '{ print $2, $4, $5 }')
+
+if [ -z $AWS_ACCESS_KEY_ID ]
+then
+  echo "Failed to get temporary credentials!"
+
+else
+  `aws --profile $AWS_USER_PROFILE configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"`
+  `aws --profile $AWS_USER_PROFILE configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"`
+  `aws --profile ec$AWS_USER_PROFILE configure set aws_session_token "$AWS_SESSION_TOKEN"`
+   
+  echo "Temporary credentials configured!"
+fi
+
